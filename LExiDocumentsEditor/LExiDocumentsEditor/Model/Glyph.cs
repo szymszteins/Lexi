7de096e9ef775466591c2144cabcb1262fa5d8b9
szymsztein <simon.mackowiak3@gmail.com>
2019-03-12 12:59:29 +0100

    Initial commit

---
+++ LExiDocumentsEditor/LExiDocumentsEditor/Model/Glyph.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace LExiDocumentsEditor.Model
+{
+    public abstract class Glyph
+    {
+        //Draw glyph on control
+        protected abstract void Draw(Control control);
+        //Insert other glyph into current glyph
+        protected abstract void Insert(Glyph glyph);
+        //Hit test agains point
+        protected abstract bool Intersect(Point point);
+    }
+}
